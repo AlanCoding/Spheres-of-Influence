2eb009e78deb93aa3301a1d311e126e4c3fcff90
Alan R <alan.rominger@gmail.com>
2016-05-05 21:59:46 -0400

    started on physical data parsing

--- soi/parsers.py
+++ soi/parsers.py
@@ -3,6 +3,9 @@ import re
 planet_list = ['Mercury', 'Venus', 'Earth', 
 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
 
+fancy_planet_list = ['Mercuran', 'Venuian', 'Earth', 
+'Martian', 'Jovian', 'Saturnian', 'Uranian', 'Neptunian', 'Pluto']
+
 with open('data/planets.txt', 'r') as f:
 	planet_text = f.read()
 
@@ -35,28 +38,59 @@ i_moon = 0
 planet_name = planet_list[0]
 
 for line in moons_orbital.split('\n'):
-	if i_planet >= len(planet_list):
-		break
-	if planet_list[i_planet] in line:
+	if i_planet < len(planet_list) and planet_list[i_planet] in line:
 		i_moon = 0
 		planet_name = planet_list[i_planet]
-		print '   planet: ' + planet_name
+		# print '   planet: ' + planet_name
 		i_planet += 1
 		moon_dict[planet_name] = {}
 		continue
 	row = re.split(r'\t', line)
 	if row[0] == '':
-		print ' continuing'
+		# print ' continuing'
 		continue
-	print ' row: ' + str(row)
+	# print ' row: ' + str(row)
 	moon_name = row[0]
-	print ' moon: ' + moon_name
+	# print ' moon: ' + moon_name
 	moon_dict[planet_name][moon_name] = {}
 	moon_dict[planet_name][moon_name]['a'] = row[4]
 	moon_dict[planet_name][moon_name]['dia'] = row[5]
+	moon_dict[planet_name][moon_name]['number'] = i_moon
+	i_moon += 1
 
 for name in planet_list:
 	print ' planet: ' + name
 	print moon_dict[name]
+
+i_planet = 0
+i_moon = 0
+
+for line in moons_physical.split('\n'):
+	for j in range(len(fancy_planet_list)):
+		fancy_planet_name = fancy_planet_list[j]
+		if fancy_planet_name in line:
+			j_save = j
+			i_planet += 1
+			i_moon = 0
+			break
+	
+	row = re.split(r'\t', line)
+	if row[0] == '' or row[1] in ('GM', ''):
+		continue
+	print ' row: ' + str(row)
+	planet_name = planet_list[j_save]
+	moon_name = row[0]
+	
+	if moon_name not in moon_dict[planet_name]:
+		print ' mismatch: ' + str(moon_name) + ' planet: ' + planet_name
+		continue
+	
+	moon_dict[planet_name][moon_name]['GM'] = row[2]
+	moon_dict[planet_name][moon_name]['r'] = row[3]
+	moon_dict[planet_name][moon_name]['rho'] = row[5]
+	moon_dict[planet_name][moon_name]['albedo'] = row[8]
+	
+# Sat.	GM		Mean radius		Mean density	Magnitude		Geometric
+#  row: ['Sinope', '0.005', '', '19', '[20]', '2.6', '18.2R', '[26]', '0.04', '']
 
 
