90472996142f19663a18c1cf29bc4cfde571dae5
Alan R <alan.rominger@gmail.com>
2016-05-10 23:20:54 -0400

    more parser preparation for calculations

--- soi/parsers.py
+++ soi/parsers.py
@@ -22,8 +22,11 @@ def parse_val(v):
 		v = 1
 	elif 'No' in v:
 		v = 0
-	return v
+	elif v == '?':
+		return None
+	return float(v)
 
+# Read planet file
 with open('data/planets.txt', 'r') as f:
 	planet_text = f.read()
 
@@ -35,20 +38,23 @@ for line in planet_text.split('\n'):
 			planet_dict[i] = {}
 			planet_dict[i]['name'] = row[i]
 	else:
+		if row[0] == 'MOON':
+			continue
 		for i in range(1, len(row)):
-			planet_dict[i-1][row[0]] = float(parse_val(row[i]))
+			key = row[0]
+			factor = 1
+			if key == 'Diameter (km)':
+				key = 'dia'
+			elif key == 'Distance from Sun (106 km)':
+				key = 'a'
+				factor = 1.0e6
+			elif key == 'Mass (1024kg)':
+				key = 'm'
+				factor = 1.0e24
+			elif key == 'Orbital Inclination (degrees)':
+				key = 'i'
+			planet_dict[i-1][key] = parse_val(row[i]) * factor
 
-# print 'planetary data by planet:'
-# for i in planet_dict:
-# 	print ' planet name: ' + planet_dict[i]['name']
-# 	print ' planet data: '
-# 	print planet_dict[i]
-
-with open('data/moons_orbital.txt', 'r') as f:
-	moons_orbital = f.read()
-
-with open('data/moons_physical.txt', 'r') as f:
-	moons_physical = f.read()
 
 moon_dict = {}
 i_planet = 0
@@ -56,11 +62,13 @@ i_moon = 0
 planet_name = planet_list[0]
 
 # moon orbital file
+with open('data/moons_orbital.txt', 'r') as f:
+	moons_orbital = f.read()
+
 for line in moons_orbital.split('\n'):
 	if i_planet < len(planet_list) and planet_list[i_planet] in line:
 		i_moon = 0
 		planet_name = planet_list[i_planet]
-		# print '   planet: ' + planet_name
 		i_planet += 1
 		moon_dict[planet_name] = {}
 		continue
@@ -81,6 +89,11 @@ i_planet = 0
 i_moon = 0
 
 # Physical data
+with open('data/moons_physical.txt', 'r') as f:
+	moons_physical = f.read()
+
+from constants import G
+
 for line in moons_physical.split('\n'):
 	for j in range(len(fancy_planet_list)):
 		fancy_planet_name = fancy_planet_list[j]
@@ -94,33 +107,16 @@ for line in moons_physical.split('\n'):
 	row = re.split(r'\t', line)
 	if row[0] == '' or row[1] in ('GM', ''):
 		continue
-	# print ' row: ' + str(row)
+
 	planet_name = planet_list[j_save]
 	moon_name = row[0].split(' ')[0]
-	
 	if moon_name not in moon_dict[planet_name]:
-		# print ' mismatch: ' + str(moon_name) + ' planet: ' + planet_name
 		continue
 	
-	if moon_name == 'Hyrokkin':
-		print row
-	
-	moon_dict[planet_name][moon_name]['GM'] = parse_val(row[1])
+	moon_dict[planet_name][moon_name]['m'] = parse_val(row[1])/G
 	moon_dict[planet_name][moon_name]['r'] = parse_val(row[3])
 	moon_dict[planet_name][moon_name]['rho'] = parse_val(row[5])
 	moon_dict[planet_name][moon_name]['albedo'] = parse_val(row[8])
-
-# Example data:
-# 0	Ganymede	Sat.
-# 1	9887.834 0.017	GM
-# 2	[8]
-# 3	2631.2 1.7	radius
-# 4	[11]
-# 5	1.942 0.005	density
-# 6	4.61 0.03	magnitude
-# 7	[14]
-# 8	0.43 0.02	albedo
-# 9	[18]
 
 
 for planet_name in copy(planet_list):
@@ -134,16 +130,15 @@ use_col = [
 	'Mass (1024kg)',
 	'Diameter (km)',
 	'Distance from Sun (106 km)',
-	'Orbital Inclination (degrees)'
+	'Orbital Inclination (degrees)',
+	'dia', 'a', 'm', 'i'
 ]
 
 print ' --planet list--'
-print '\t'.join([k for k in planet_dict[0].keys() if k in use_col])
+print 'name\t' + '\t'.join([k for k in planet_dict[0].keys() if k in use_col])
 for j in range(len(planet_dict)):
-	print '\t'.join([str(planet_dict[j][k]) for k in planet_dict[0].keys() if k in use_col])
-
-from constants import G
-print ' G: ' + str(G)
+	print (planet_dict[j]['name'] + '\t' + '\t'.join([str(planet_dict[j][k])
+		   for k in planet_dict[0].keys() if k in use_col]))
 
 print '\n--moon data--'
 
@@ -152,5 +147,5 @@ print 'moon_name\tplanet_name\t' + '\t'.join(moon_fields)
 for planet_name in planet_list:
 	for moon_name in moon_dict[planet_name]:
 		print (moon_name + '\t' + planet_name + '\t' + 
-		'\t'.join([str(moon_dict[planet_name][moon_name][k]) for k in
-		moon_dict[planet_name][moon_name].keys() if k in moon_fields]))
+			   '\t'.join([str(moon_dict[planet_name][moon_name][k]) for k in
+			   moon_dict[planet_name][moon_name].keys() if k in moon_fields]))
